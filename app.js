813c6728d56403b58cc27fcb6071985d19cc6cf7
julichavarria <juliichavarriavillagran@gmail.com>
2022-04-26 20:19:57 -0300

    enlaces paginas register, productDetail, login y carrito

--- app.js
+++ app.js
@@ -14,5 +14,10 @@ app.get ("/", (req,res)=> res.sendFile(path.join(__dirname, "./public/css/styles
 
 app.get ("/", (req,res)=> res.sendFile(path.join(__dirname, "./public/css/normalize.css" ) ));
 
-app.get ("/register.html", (req,res)=> res.sendFile(path.join(__dirname, "./views/register.html" ) ));
-app.get ("/productDetail.html", (req,res)=> res.sendFile(path.join(__dirname, "./views/productDetail.html" ) ));
+app.get ("/register", (req,res)=> res.sendFile(path.join(__dirname, "./views/register.html" ) ));
+
+app.get ("/productDetail", (req,res)=> res.sendFile(path.join(__dirname, "./views/productDetail.html" ) ));
+
+app.get ("/login", (req,res)=> res.sendFile(path.join(__dirname, "./views/login.html" ) ));
+
+app.get ("/carrito", (req,res)=> res.sendFile(path.join(__dirname, "./views/carrito.html" ) ));
