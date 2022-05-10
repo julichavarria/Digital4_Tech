36c0ef3bb9be38afd63083b2e6a43dd15b029f8b
julichavarria <juliichavarriavillagran@gmail.com>
2022-05-09 23:06:21 -0300

    rutas

---
+++ controllers/productosController.js
@@ -0,0 +1,50 @@
+const res = require("express/lib/response")
+
+let pcArmadas = [
+{
+    titulo: "PC GAMER GM CARBIDE 1",
+    imagen: "/img/productos_pcArmadas/pc1.jpeg" ,
+    prcesador:"INTEL CORE I5 11600F",
+    mother: "GIGABYTE B560M-DS3H",
+    placaDeVideo: "MSI GE FORCE GTX1050TI OC 4GB GDDR5",
+    memoriaRam: "16GB KINGTONG DDR4 2666MHZ",
+    discoRigido:"240GB SSD DISCO DE ESTADO SÓLIDO WD",
+    gabinete:"GAMER RGB LED AEROCOOL BOLT",
+    fuente:"AEROCOOL CYLON 500W RGB CON 80 PLUS BRONZE",
+}, 
+{
+    titulo: "PC GAMER GM CARBIDE 2",
+    imagen: "jjjjj" ,
+    prcesador:"INTEL CORE I5 11600F",
+    mother: "GIGABYTE B560M-DS3H",
+    placaDeVideo: "MSI GE FORCE GTX1050TI OC 4GB GDDR5",
+    memoriaRam: "16GB KINGTONG DDR4 2666MHZ",
+    discoRigido:"240GB SSD DISCO DE ESTADO SÓLIDO WD",
+    gabinete:"GAMER RGB LED AEROCOOL BOLT",
+    fuente:"AEROCOOL CYLON 500W RGB CON 80 PLUS BRONZE",
+},
+{
+    titulo: "PC GAMER GM CARBIDE 3",
+    imagen: "jjjjj" ,
+    prcesador:"INTEL CORE I5 11600F",
+    mother: "GIGABYTE B560M-DS3H",
+    placaDeVideo: "MSI GE FORCE GTX1050TI OC 4GB GDDR5",
+    memoriaRam: "16GB KINGTONG DDR4 2666MHZ",
+    discoRigido:"240GB SSD DISCO DE ESTADO SÓLIDO WD",
+    gabinete:"GAMER RGB LED AEROCOOL BOLT",
+    fuente:"AEROCOOL CYLON 500W RGB CON 80 PLUS BRONZE",
+},
+{
+    titulo: "PC GAMER GM CARBIDE 4",
+    imagen: "jjjjj" ,
+    prcesador:"INTEL CORE I5 11600F",
+    mother: "GIGABYTE B560M-DS3H",
+    placaDeVideo: "MSI GE FORCE GTX1050TI OC 4GB GDDR5",
+    memoriaRam: "16GB KINGTONG DDR4 2666MHZ",
+    discoRigido:"240GB SSD DISCO DE ESTADO SÓLIDO WD",
+    gabinete:"GAMER RGB LED AEROCOOL BOLT",
+    fuente:"AEROCOOL CYLON 500W RGB CON 80 PLUS BRONZE",
+}
+]  
+//EXPORTAR // 
+// res.render("index", )
