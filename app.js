775f2dfe50bfa9d17384c3c115dfdd649275ca9d
julichavarria <juliichavarriavillagran@gmail.com>
2022-05-09 23:05:02 -0300

    rutas

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ const app = express ();
 
 const path = require ("path");
 
+app.set ("view engine", "ejs");
+
+const publicPath = path.resolve ( __dirname, "./public");
+
 app.use(express.static("public"));
 
+const routerMain = require ("./routes/main");
+
+app.use ("/", routerMain);
+
 app.listen(3030, ()=> console.log ("Server running in 3030 port ") );
-
-app.get ("/", (req,res)=> res.render(path.join(__dirname, "./views/index.ejs" ) ));
-
-app.get ("/index", (req,res)=> res.render(path.join(__dirname, "./views/index.ejs" ) ));
-
-app.get ("/register", (req,res)=> res.render(path.join(__dirname, "./views/users/register.ejs" ) ));
-
-app.get ("/productDetail", (req,res)=> res.render(path.join(__dirname, "./views/product/productDetail.ejs" ) ));
-
-app.get ("/login", (req,res)=> res.render(path.join(__dirname, "./views/users/login.ejs" ) ));
-
-app.get ("/productCart", (req,res)=> res.render(path.join(__dirname, "./views/product/productCart.ejs" ) ));
