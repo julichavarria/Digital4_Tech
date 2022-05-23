e4efddc89367b1293402cfd245f350852a020e58
julichavarria <juliichavarriavillagran@gmail.com>
2022-05-23 19:34:03 -0300

    base datos usuarios js

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,13 +1,102 @@
-// let usuarios [
-//     {
-//         id:
-//         nombre: 
-//         apellido: 
-//         usuario:
-//         email: 
-//         contraseña: 
-//         rol: admin/cliente
-//         avatar: seleccionar de una base, o subir
-
-// }
-// ]
+let usuarios = [
+    {
+        id: 1,
+        nombre: "Martin",
+        apellido: "Gonzales",
+        usuario: "Tincho01",
+        email: "martin_gonzales@gmail.com",
+        contraseña: "E41F56G0",
+        rol: "administrador",
+        avatar: null,
+    },
+    {
+        id: 2,
+        nombre: "Eduardo",
+        apellido: "Parra",
+        usuario: "EduParra",
+        email: "edu.parra@gmail.com",
+        contraseña: "E415G450",
+        rol: "cliente",
+        avatar: null,
+    },
+    {
+        id: 3,
+        nombre: "Sofia",
+        apellido: "Ramos",
+        usuario: "SofiRamos20",
+        email: "sofia_ramos@gmail.com",
+        contraseña: "G6G0123",
+        rol: "administrador",
+        avatar: null,
+    },
+    {
+        id: 4,
+        nombre: "Sebastian",
+        apellido: "Juarez",
+        usuario: "Sebas2022",
+        email: "sebas_juarez@gmail.com",
+        contraseña: "YYY45678",
+        rol: "cliente",
+        avatar: null,
+    },
+    {
+        id: 5,
+        nombre: "Juana",
+        apellido: "Acosta",
+        usuario: "JAcosta",
+        email: "juana-acosta@gmail.com",
+        contraseña: "258GEX654",
+        rol: "cliente",
+        avatar: null,
+    },
+    {
+        id: 6,
+        nombre: "Ricardo",
+        apellido: "Tedin",
+        usuario: "Richard25",
+        email: "richard.tedin@gmail.com",
+        contraseña: "CJ978HY563",
+        rol: "cliente",
+        avatar: null,
+    },
+    {
+        id: 7,
+        nombre: "Martina",
+        apellido: "Taboada",
+        usuario: "Martu-26",
+        email: "martina.taboada@gmail.com",
+        contraseña: "JL456CH74",
+        rol: "cliente",
+        avatar: null,
+    },
+    {
+        id: 8,
+        nombre: "Pilar",
+        apellido: "Martinez",
+        usuario: "Pily090",
+        email: "pily_martinez@gmail.com",
+        contraseña: "F4123F5",
+        rol: "cliente",
+        avatar: null,
+    },
+    {
+        id: 9,
+        nombre: "Romina",
+        apellido: "Norte",
+        usuario: "Romi.Nor",
+        email: "romi-norte@gmail.com",
+        contraseña: "25TY45TG6",
+        rol: "administrador",
+        avatar: null,
+    },
+    {
+        id: 10,
+        nombre: "Virginia",
+        apellido: "Goytia",
+        usuario: "Vicky19",
+        email: "vicky.g19@gmail.com",
+        contraseña: "MA4545TR4",
+        rol: "cliente",
+        avatar: null,
+    },
+    ]
