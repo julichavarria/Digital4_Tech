eb5a5ee11bedbb3d3406419225c25b18f2495ff3
Walter Adrián Olivera <adrian.olivera.11@gmail.com>
2022-08-19 23:13:08 -0300

    integracion api

--- dashboard/src/components/Content.jsx
+++ dashboard/src/components/Content.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import Header from "./Header";
 import DataContainer from "./DataContainer";
 
 
-function Content (){
-    return(
-        <div>
-           <Header/>
-           <DataContainer/>
-        </div>
-    )
+class Content extends Component {
+    render (){
+        return(
+            <div>
+            <Header/>
+            <DataContainer/>
+            </div>
+        )
+    }
 }
 
 export default Content;
-
-//  HEADER
-// DATA CONTAINER
--- dashboard/src/components/Counter.jsx
+++ dashboard/src/components/Counter.jsx
@@ -1,56 +1,65 @@
 import React, { Component } from 'react';
 import icon from "../assets/images/icon_products.svg"
-import propTypes from "propTypes"
+//import propTypes from "propTypes"
 
 
 class Counter extends Component {
-    constructor(props ) {
-        super (props);
+    constructor( ) {
+        super ();
         this.state = {
-        PcMarcas: "" 
+        PcMarcas: '',
+        totalPc: '',
+        }
     }
-}
-apiCall (url, consecuencia){
-    fetch (url)
-        .then( response => response.json())
-        .then ( data => consecuencia(data))
-        .chatch ( error => console.log (error))
-        
-}
-    componentDidMount (){
-        console.log("estas aqui!")
-        this.apiCall("http://localhost:3030/api/products", this.datosProducts)
-            
-            }
-    datosProducts = (data) =>{
-        console.log (data.countByCategory.Amd)
-        // this.setState (
-        //     { 
-        //         PcMarcas: data.data
-        // })
-    }
-            render () {
-                console.log("estoy funcionando")
 
-    return (
-        <div className="contenedorDatosProductos">
-                <h1> <img src={icon} alt="icono productos"/>PC ARMADAS</h1>
-                <div className="contenedorContador">
-                    <div className="contador">30</div>
-                    <div>
-                        <p><span>{ this.state.PcMarcas}</span>INTEL</p>
-                        <hr/>
-                        <p><span>15</span>AMD</p>
+    componentDidMount(){
+        fetch('http://localhost:3030/api/products')
+            .then(response => response.json())
+            .then(data => {this.setState({PcMarcas: data.countByCategory, totalPc: data.total})})
+            .catch(e => {console.log(e)})
+        }
+
+    // apiCall (url, consecuencia){
+    //     fetch (url)
+    //         .then( response => response.json())
+    //         .then ( data => consecuencia(data))
+    //         .chatch ( error => console.log (error))
+            
+    // }
+
+    // componentDidMount (){
+    //     this.apiCall("http://localhost:3030/api/products", this.datosProducts2)
+    // }
+
+    // datosProducts2 = (data) => {
+    //     return 'datosProducts2'
+    // }
+
+    // datosProducts = (data) =>{
+    //     console.log (data)
+    //     this.setState (
+    //         { 
+    //             PcMarcas: 'hola PcMacas'
+                
+    //     })
+    // }
+
+    render () {
+        return (
+            <div className="contenedorDatosProductos">
+                    <h1> <img src={icon} alt="icono productos"/>PC ARMADAS</h1>
+                    <div className="contenedorContador">
+                        <div className="contador">{this.state.totalPc}</div>
+                        <div>
+                            <p><span> {this.state.PcMarcas.Intel} </span>INTEL</p>
+                            <hr/>
+                            <p><span>{this.state.PcMarcas.Amd}</span>AMD</p>
+                        </div>
                     </div>
-                </div>
-            </div>  
+                </div>  
             );
         };
     }
 
-Counter.propTypes = {
-    
-
-}
 export default Counter;
 
