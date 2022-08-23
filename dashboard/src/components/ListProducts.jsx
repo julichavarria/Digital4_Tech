2d36db8f9ea41facb15f0bfaecde181623d10f1c
Walter Adrián Olivera <adrian.olivera.11@gmail.com>
2022-08-22 23:43:46 -0300

    paginado dashboad productos

--- dashboard/src/components/ListProducts.jsx
+++ dashboard/src/components/ListProducts.jsx
@@ -6,6 +6,8 @@ class ListProducts extends Component {
         super (props);
         this.state = {
             listadoRegistro: '',
+            inicio: 0,
+            fin: 5,
         }
     }
 
@@ -24,6 +26,24 @@ class ListProducts extends Component {
         let dataList = Object.values(this.state.listadoRegistro);
         return dataList;
     }
+
+    previousPage(){
+        if (this.state.inicio >= 5 ){
+            this.setState({
+                inicio: this.state.inicio -= 5,
+                fin: this.state.fin -= 5
+            })
+        }
+    }
+    nextPage(){
+        if (this.state.fin <= this.array().length ){
+            this.setState({
+                inicio: this.state.inicio += 5,
+                fin: this.state.fin += 5
+            })
+        }
+    }
+
     listar(){
         let salida = this.array().map( (produt, i) => 
             <div className="contenedorItemListado">
@@ -35,9 +55,10 @@ class ListProducts extends Component {
                     <a href={`http://localhost:3030/products/editProduct/${this.array()[i].id}`}><i className="far fa-edit"></i></a>
                     <a href={`http://localhost:3030/products/confirmDelete/${this.array()[i].id}`}><i className="fa-regular fa-trash-can"></i></a>
                 </div>
+            
             </div>
-            )
-        return salida
+        )
+        return salida.slice(this.state.inicio, this.state.fin)
     }
 
 render () {
@@ -45,6 +66,8 @@ render () {
         <div className="contenedorListado">           
             <h2>LISTADO DE PRODUCTOS</h2>
             <div>{this.listar()}</div>
+            <a onClick={ ()=> this.previousPage() }>Pagina Anterior</a>
+            <a onClick={ ()=> this.nextPage() }>Pagina Siguiente</a>
         </div> 
         );
     };
